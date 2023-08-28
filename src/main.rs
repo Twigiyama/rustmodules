ea4b88e832679bb1980a721dd08b87743a6231e3
Asitha Rodrigo <asitharodrigo@Asithas-MacBook-Pro-2023.local>
2023-08-28 11:36:57 +0100

    Adding randomn number generator crate

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::io;
+use rand::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    let mut buffer = String::new();
+    println!("Enter a new line of text:");
+    io::stdin().read_line(&mut buffer);
+    println!("The text you entered is {buffer}");
+
+    let number: i32 = buffer.trim().parse().unwrap();
+    println!("number + 1 is {}", number + 1);
+
+    let number = random::<f64>();
+    println!("number is {}", number);
+
+    let number = thread_rng().gen_range(1..11);
+    println!("number is {}", number);
 }
